d4a1c92f7cf9d397338ae79773055044562e14df
Udit <codenirvana28@gmail.com>
2016-02-04 12:38:23 +0530

    coming soon

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Hello
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Renaissance 16</title>
+        <style media="screen">
+            body{
+                background: #000000 url("r16.png") no-repeat fixed center;
+            }
+        </style>
+    </head>
+    <body>
+    </body>
+</html>
