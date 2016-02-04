29dcfc5db9e053017266b6237109b157ac7359da
Udit <codenirvana28@gmail.com>
2016-02-04 20:55:04 +0530

    api minor changes

--- api/index.php
+++ api/index.php
@@ -5,20 +5,18 @@ require '../inc/connection.php';
 
 $app = new Slim\App(['settings' => ['displayErrorDetails' => true]]);
 
-
 $app->get('/',function($request, $response, $args){
-    $response->write("Welcome to my api");
+    $response->write("JECRC Renaissance 16 API");
 });
 
 // get all the categories with details
 $app->get('/events/categories','getCategories');
 
 // get the details and events for a particular category
-
 $app->get('/events/categories/{id}','getEventByCategory');
 
 //get all the events
-$app->get('/events','getEventList');
+$app->get('/events','getEvents');
 
 //get event details of particular event
 $app->get('/events/{id}','getEvent');
@@ -28,6 +26,7 @@ function getEvent($request, $response, $args){
     $id = $args['id'];
     $q = $db->prepare("SELECT * FROM Events JOIN EventDetails ON Events.id = EventDetails.id  WHERE Events.id=?");
     $q->execute(array($id));
+
     $json = json_encode($q->fetch(PDO::FETCH_ASSOC));
     $response->write($json);
 }
@@ -43,19 +42,16 @@ function getEventByCategory($request, $response, $args){
     $events->execute(array($id));
     $events = $events->fetchAll(PDO::FETCH_ASSOC);
 
-
     $json = json_encode(array("details"=> $categories ,"events"=> $events));
     $response->write($json);
 }
 
-function getEventList($request, $response, $args){
+function getEvents($request, $response, $args){
     global $db;
-
     $categories = $db->query("SELECT * FROM EventCategories");
     $categories = $categories->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_UNIQUE|PDO::FETCH_ASSOC);
 
     $events = $db->query("SELECT * FROM Events JOIN EventDetails ON Events.id = EventDetails.id");
-    //$events = $events->fetchAll(PDO::FETCH_ASSOC);
 
     while($row = $events->fetch(PDO::FETCH_ASSOC)){
         $categories[$row['category']]['events'][] = $row;
@@ -67,12 +63,11 @@ function getEventList($request, $response, $args){
 
 function getCategories($request, $response, $args){
     global $db;
-
     $categories = $db->query("SELECT * FROM EventCategories");
     $categories = $categories->fetchAll(PDO::FETCH_ASSOC);
+
     $json = json_encode($categories);
     $response->write($json);
 }
-
 
 $app->run();
