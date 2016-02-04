1230d755490ab03a23194f8cb6df2eb31e84313f
Udit <codenirvana28@gmail.com>
2016-02-04 14:34:41 +0530

    changed config

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   define("ROOT_PATH",$_SERVER['DOCUMENT_ROOT']);
-
-   //DataBase Information
-   define('DB_NAME','ren16');
-   define('DB_HOST','localhost');
-   define('DB_USERNAME','root');
-   define('DB_PASSWORD','toor');
