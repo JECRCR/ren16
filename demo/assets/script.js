fa0c52624b199c3d9fe4d8ae422fc1bc69b5054d
Udit <codenirvana28@gmail.com>
2016-02-06 12:31:27 +0530

    tabed events

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -1,13 +1,15 @@
 $(".bar").click(function(){
     var that = $(this);
-    that.find('.inner-bar img').hide();
+    that.find('.inner-bar .category-img').hide();
     that.addClass('bar-full');
     that.siblings().addClass('zero-width');
+    that.find('.event-content').show();
 });
 $(".menu-button").click(function(){
   if($(".bar").hasClass('bar-full')){
       $(".bar").removeClass('bar-full');
       $('.bar').removeClass('zero-width');
+      $('.bar').find('.event-content').hide();
       $('.inner-bar img').fadeIn();
   } else {
     $(".main-page").addClass('main-page-show');
