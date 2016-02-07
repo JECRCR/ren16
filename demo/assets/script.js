b0c1f7921aa86234af647f2f2eb23ef86b24755e
Udit <codenirvana28@gmail.com>
2016-02-07 10:26:18 +0530

    beautify code

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -1,24 +1,24 @@
-$(".bar").click(function(){
+$(".bar").click(function() {
     var that = $(this);
     that.find('.inner-bar .category-img').hide();
     that.addClass('bar-full');
     that.siblings().addClass('zero-width');
     that.find('.event-content').show();
 });
-$(".menu-button").click(function(){
-  if($(".bar").hasClass('bar-full')){
-      $(".bar").removeClass('bar-full');
-      $('.bar').removeClass('zero-width');
-      $('.bar').find('.event-content').hide();
-      $('.inner-bar img').fadeIn();
-  } else {
-    $(".main-page").addClass('main-page-show');
-  }
+$(".menu-button").click(function() {
+    if ($(".bar").hasClass('bar-full')) {
+        $(".bar").removeClass('bar-full');
+        $('.bar').removeClass('zero-width');
+        $('.bar').find('.event-content').hide();
+        $('.inner-bar img').fadeIn();
+    } else {
+        $(".main-page").addClass('main-page-show');
+    }
 });
-$(".explore-button").click(function(){
+$(".explore-button").click(function() {
     $(".main-page-show").removeClass("main-page-show");
 });
-$(".event-icon").click(function(){
+$(".event-icon").click(function() {
     var that = $(this);
     $(".event-details").slideDown();
 });
