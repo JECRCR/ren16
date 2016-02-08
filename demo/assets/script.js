183d4ab7171ba670cce9079ace84ce6038cb138c
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-02-08 13:01:24 +0530

    event tabs

--- demo/assets/script.js
+++ demo/assets/script.js
@@ -25,5 +25,20 @@ $(".event-icon").click(function() {
     $(".event-details").slideDown();
 });
 $(".event-details").click(function() {
-    $(this).slideUp();
+    //$(this).slideUp();
 });
+
+/*TABS*/
+
+$(document).ready(function() {
+    $("div.vertical-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.vertical-tab>div.vertical-tab-content").removeClass("active");
+        $("div.vertical-tab>div.vertical-tab-content").eq(index).addClass("active");
+    });
+});
+
+/*TABS END*/
