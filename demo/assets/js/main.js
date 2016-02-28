c19414711401d57b15bdc7b60029094b1ed5d24e
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-02-28 16:28:29 +0530

    Gallery added

--- demo/assets/js/main.js
+++ demo/assets/js/main.js
@@ -72,7 +72,56 @@ renApp.config(function($stateProvider, $urlRouterProvider,$locationProvider) {
             url: '/gallery',
             templateUrl: 'assets/partials/partial-gallery.html',
             controller: function($scope, $state){
+                $scope.$on('$viewContentLoaded', function() {
+                    var images = [];
+                    for(i=1;i<50;i++)
+                        images.push(i+".jpg");
+                    console.log(images);
 
+                    $scope.images = images;
+                    var $gallery = $('.gallery a').simpleLightbox();
+
+                    $gallery.on('show.simplelightbox', function () {
+                        console.log('Requested for showing');
+                    })
+                        .on('shown.simplelightbox', function () {
+                            console.log('Shown');
+                        })
+                        .on('close.simplelightbox', function () {
+                            console.log('Requested for closing');
+                        })
+                        .on('closed.simplelightbox', function () {
+                            console.log('Closed');
+                        })
+                        .on('change.simplelightbox', function () {
+                            console.log('Requested for change');
+                        })
+                        .on('next.simplelightbox', function () {
+                            console.log('Requested for next');
+                        })
+                        .on('prev.simplelightbox', function () {
+                            console.log('Requested for prev');
+                        })
+                        .on('nextImageLoaded.simplelightbox', function () {
+                            console.log('Next image loaded');
+                        })
+                        .on('prevImageLoaded.simplelightbox', function () {
+                            console.log('Prev image loaded');
+                        })
+                        .on('changed.simplelightbox', function () {
+                            console.log('Image changed');
+                        })
+                        .on('nextDone.simplelightbox', function () {
+                            console.log('Image changed to next');
+                        })
+                        .on('prevDone.simplelightbox', function () {
+                            console.log('Image changed to prev');
+                        })
+                        .on('error.simplelightbox', function (e) {
+                            console.log('No image found, go to the next/prev');
+                            console.log(e);
+                        });
+                });
             }
         })
         .state('sponsors', {
